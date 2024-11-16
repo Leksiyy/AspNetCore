f2c6d36c94939c5b83f75729aaba63f15c8d4989
Leksiyy <leksiy197618@gmail.com>
2024-11-16 11:34:41 +0200

    hw1 complete

--- homework1/Models/Person.cs
+++ homework1/Models/Person.cs
@@ -2,5 +2,7 @@ namespace homework1;
 
 public class Person
 {
-    
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
 }
--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,29 +1,44 @@
+using homework1;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-
+List<Person> persons = new List<Person>();
 
 app.Run(async (context) =>
 {
     context.Response.ContentType = "text/html; charset=utf-8";
 
-    if (context.Request.Path == "/postuser")
+    if (context.Request.Path == "/")
+    {
+        await context.Response.WriteAsync("<h1 style=\"color: red\">Hello from this page</h1>\n" +
+                                          $"<a href=\"/postuser\">invite</a>");
+    }
+    else if (context.Request.Path == "/postuser" && context.Request.Method == "GET")
+    {
+        await context.Response.WriteAsync(@"<h2>User form</h2>
+            <form method=""post"" action=""/posted"">
+            <p>Name: <input name=""name"" /></p>
+            <p>Email: <input name=""email"" /></p>
+            <p>Phone: <input name=""phone"" /></p>
+            <input type=""submit"" value=""Send"" />
+            </form>");
+    }
+    else if (context.Request.Path == "/posted" && context.Request.Method == "POST")
     {
         var form = context.Request.Form;
-        
+
         var name = form["name"];
         var email = form["email"];
         var phone = form["phone"];
 
-        await context.Response.WriteAsync($"<div>" +
-                                          $"<p>Name: {name}</p>" +
-                                          $"<p>Email: {email}</p>" +
-                                          $"<p>Phone number: {phone}</p>" +
-                                          $"</div>");
+        persons.Add(new Person{Email = email, Name = name, Phone = phone});
+
+        await context.Response.WriteAsync("<h1 style=\"color: green\">Thank you for registration</h1>");
     }
     else
     {
-        await context.Response.SendFileAsync("html/index.html");
+        await context.Response.WriteAsync("There is nothing here.");
     }
 });
 
